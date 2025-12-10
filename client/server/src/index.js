14e34619a30cbecd3cce3dfaa81bf3454a9d21f1
Luis <galapon.luismiguel.paterno@gmail.com>
2025-12-11 00:47:09 +0800

    Add production domain to CORS whitelist

--- client/server/src/index.js
+++ client/server/src/index.js
@@ -11,7 +11,13 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://127.0.0.1:5173', 'http://127.0.0.1:5174'],
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'http://127.0.0.1:5173',
+    'http://127.0.0.1:5174',
+    'https://royalwings.vercel.app'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
